800130ce4f0e16129c13c9a735df5fb898ec9d55
Yousef Meska <mrbomberboy123@gmail.com>
2022-04-05 17:12:35 +0200

    completes registeration, login and cart pages

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # Food-Delivery-App
-✨Food Ordering application built using Node.Js and vanilla javascript
+
+☘️ Food Ordering application built using Node.Js and vanilla javascript
 
 ## Project structure
 
 
+## scripts
+- build 
+```sh
+
+```
+
+- test
+```sh
+```
+
 ## Screenshots
+admin panel 
+
+
+
+customer panel
+
+
+
 
 
 ## dependencies
-
 - Laravel-mix
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-//const expressLayout = require("express-ejs-layouts");
+const expressLayout = require("express-ejs-layouts");
 const path = require("path");
 require("dotenv").config();
 
 const { PORT } = Number(process.env) || 4000;
 console.log(PORT);
 // Templates
-//app.use(expressLayout);
+app.use(expressLayout);
 app.use(express.static("public"));
 app.set("views", path.join(__dirname, "../resources/views"));
 app.set("view engine", "ejs");
@@ -15,6 +15,16 @@ app.set("view engine", "ejs");
 
 app.get("/home", (req, res) => {
   res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+app.get("/register", (req, res) => {
+  res.render("auth/register");
 });
 app.listen(3000, () => {
   console.log(`🚀 server is running on: http://localhost:${3000}`);
